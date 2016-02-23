2aa8b5d14edcbf566f41a8173872d41bb62995ef
FSEVERI\marchiori2904 <FSEVERI\marchiori2904@lmatbi-17.fseveri.it>
2016-02-23 08:45:09 +0100

    Test commit

---
+++ src/schooltest/SchoolTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schooltest;
+
+/**
+ *
+ * @author FSEVERI\marchiori2904
+ */
+public class SchoolTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
